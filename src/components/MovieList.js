c32a8bc8ec9cd7d96bba4cb7cdc89d668625a340
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2020-02-23 01:06:10 +0100

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,16 +4,16 @@ import Card from './Card';
 import shortid from 'shortid';
 
 const movies = [
-  { title: "Jurassic Park", director: "Steven Spielberg" },
-  { title: "Sharknado", director: "Anthony C. Ferrante" },
-  { title: "Titanic", director: "James Cameron" }
+  { _id:"2143lkjgfw8", title: "Jurassic Park", director: "Steven Spielberg" },
+  { _id:"jg59lkjgby5", title: "Sharknado", director: "Anthony C. Ferrante" },
+  { _id:"a45i3ljamv2", title: "Titanic", director: "James Cameron" }
 ];
 
 const MovieList = () =>{
   return (
     <ul>
       { 
-        movies.map( (oneMovie, index) => <Card key={shortid.generate()} movie={oneMovie} /> ) 
+        movies.map( (oneMovie, index) => <Card key={ movieObj._id } movie={oneMovie} /> ) 
       }
     </ul>
   )
