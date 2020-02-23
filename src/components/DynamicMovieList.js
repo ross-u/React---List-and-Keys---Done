d2933168f71577303c9a71e329f3fdafd17006a2
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2020-02-23 01:04:50 +0100

    Update DynamicMovieList.js

--- src/components/DynamicMovieList.js
+++ src/components/DynamicMovieList.js
@@ -1,40 +1,42 @@
 // src/components/DynamicMovieList.js
-import React, { Component } from 'react';
-import ImprovedCard from './ImprovedCard';
+import React, { Component } from "react";
+import ImprovedCard from "./ImprovedCard";
 
 class DynamicMovieList extends Component {
-  //  we use `constructor` as we are setting initial `state` value from the `props`
+  //  we use `constructor` as we pass movies through props to set the as `state` value
+  //  Even though  there is a use case for this pattern,
+  //  Setting initial state from the props is an anti-pattern,
+  //	and `componentDidUpdate` lifecycle method should be used insted
   constructor(props) {
     super();
     this.state = {
       movies: props.moviesArray
-    }
+    };
   }
 
-  deleteMovie = (movieIndex) => {
-    const moviesCopy = this.state.movies;
-    moviesCopy.splice(movieIndex, 1);
-    this.setState( {movies: moviesCopy} );
-  }
+  
+  deleteMovie = movieId => {
+    const moviesCopy = this.state.movies.filter(movie => movie._id !== movieId);
+
+    this.setState({ movies: moviesCopy });
+  };
+
 
   render() {
     return (
       <ul>
-        { 
-          this.state.movies.map( (oneMovie, index) => {
-            return (
-              <ImprovedCard 
-                key={index} 
-                {...oneMovie}
-                clickToDelete={ ()=> this.deleteMovie(index)}
-            //  clickToDelete={this.deleteMovie.bind(this, index)}
-              />
-            ) 
-          })
-        }
+        {this.state.movies.map((oneMovie, index) => {
+          return (
+            <ImprovedCard
+              key={oneMovie._id}
+              {...oneMovie}
+              clickToDelete={() => this.deleteMovie(oneMovie._id)}
+            />
+          );
+        })}
       </ul>
-    )
+    );
   }
 }
 
-export default DynamicMovieList; 
+export default DynamicMovieList;
